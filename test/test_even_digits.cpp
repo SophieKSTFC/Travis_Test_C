7556cff996584aae6b9a7652816e3b3a7452cb45
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-14 13:38:27 +0100

    added test files

---
+++ test/test_even_digits.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE test_even_digits
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN  // in only one cpp file
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(my_test){
+
+    BOOST_CHECK(1 + 1 == 2);
+}
